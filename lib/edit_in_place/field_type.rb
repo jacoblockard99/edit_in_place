d2da35fd0bfe0de7734b7eb88b179874437634a0
Jacob <jacoblockard99@gmail.com>
2021-07-06 22:02:06 -0400

    Improve FieldType
    Improve FieldType to make it much easier to extend without re-writing a
    lot of common functionality.

--- lib/edit_in_place/field_type.rb
+++ lib/edit_in_place/field_type.rb
@@ -7,21 +7,48 @@ module EditInPlace
   # @author Jacob Lockard
   # @since 0.1.0
   class FieldType
-    # @!method render(options, args)
-    #   Render the field given a {FieldOptions} instance and an array of arguments passed by
-    #   the caller.
-    #   @param options [FieldOptions] options passed by the {Builder} instance that should
-    #     be used to render the field.
-    #   @param args [Array<Object>] the arguments passed by the field creator.
-    #   @return [String] the rendered HTML.
-    #   @abstract All subclasses must implement #render.
+    # Render the field given a {FieldOptions} instance and an array of arguments passed by
+    # the caller.
+    #
+    # While subclasses may override this method as appropriate, the default
+    # implementation simply does two things:
+    # 1. uses {#validate_mode!} to ensure that the mode is supported, and
+    # 2. calls a +render_*+ method.
+    # For example, if the mode were +:admin_editing+, then a +render_admin_editing+ method would
+    # be called. Naturally, the +render_*+ methods need to be defined by the subclass.
+    # @param options [FieldOptions] options passed by the {Builder} instance that should
+    #   be used to render the field.
+    # @param args [Array<Object>] the arguments passed by the field creator.
+    # @return [String] the rendered HTML.
+    def render(options, *args)
+      validate_mode!(options.mode)
+      send("render_#{options.mode}", options, *args)
+    end
+
+    # Gets the modes that are supported by this field type, +:viewing+ and +:editing+ by default.
+    # @return [Array<Symbol>] the modes supported by this field type.
+    # @note Subclasses should override this method as appropriate.
+    def supported_modes
+      [:viewing, :editing]
+    end
 
     # Creates a deep copy of this {FieldType} that can be safely modified.
     # @return [FieldType] a deep copy of this field type.
-    # @note This method should be overrided as necessary by sub-classes to duplicate any added
+    # @note This method should be overriden as necessary by subclasses to duplicate any added
     #   data.
     def dup
       super
+    end
+
+    protected
+
+    # Ensures that the given mode is supported by this field type.
+    # @param mode [Symbol] the mode to validate.
+    # @return [void]
+    def validate_mode!(mode)
+      unless supported_modes.include? mode
+        raise "The mode '#{mode}' is not supported by this field type!"
+      end
     end
   end
 end
--- spec/test_field_type.rb
+++ spec/test_field_type.rb
@@ -5,12 +5,18 @@ class TestFieldType < EditInPlace::FieldType
     @arg = arg
   end
 
-  def render
-    'Rendered!'
-  end
-
   def dup
     TestFieldType.new(arg.dup)
+  end
+
+  protected
+
+  def render_viewing(options, *args)
+    "Init: #{arg}, After: #{args.first}"
+  end
+
+  def render_editing(options, *args)
+    "EDITING: Init: #{arg}, After: #{args.first}"
   end
 end
 
