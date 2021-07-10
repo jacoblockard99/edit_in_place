29dfc3f38902290d2323cc92838943d2335820e4
Jacob <jacoblockard99@gmail.com>
2021-07-09 22:46:40 -0400

    Add code coverage
    Install and use simplecov, adding its 'coverage' directory to the
    .gitignore, and bring code coverage up to 100.0%.

--- spec/support/another_extended_builder.rb
+++ spec/support/another_extended_builder.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class AnotherExtendedBuilder < EditInPlace::ExtendedBuilder
-  def another_extension(name)
-    "Hello, #{name}!"
-  end
+  def another_extension(name); end
 end
--- spec/support/test_extended_builder.rb
+++ spec/support/test_extended_builder.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class TestExtendedBuilder < EditInPlace::ExtendedBuilder
-  def an_extension
-    field TestFieldType.new('An Extended Field!')
-  end
+  def an_extension; end
 end
