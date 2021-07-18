5577b3f7caf4cc069b4063b353bcd4ab1e601f85
Jacob <jacoblockard99@gmail.com>
2021-07-17 22:02:14 -0400

    Correct MiddlewareRegistar docs
    Correct typos in the MiddlewareRegistrar documentation.

--- lib/edit_in_place/middleware_registrar.rb
+++ lib/edit_in_place/middleware_registrar.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 module EditInPlace
-  # {MiddlewareRegistrar] is a subclass of {Registrar] that only allows middleware objects (as
-  # defined by {Middlegem::Middleware.valid?}) to be registered.
+  # {MiddlewareRegistrar} is a subclass of {Registrar} that only allows middleware objects (as
+  # defined by +Middlegem::Middleware.valid?+) to be registered.
   #
   # @author Jacob Lockard
   # @since 0.2.0
   class MiddlewareRegistrar < Registrar
     protected
 
-    # Adds to the default `validate_registration!` implementation by ensuring that only
-    # middleware objects (as defined by {Middlegem::Middleware.valid?}) can be registered.
+    # Adds to the default +validate_registration!+ implementation by ensuring that only
+    # middleware objects (as defined by +Middlegem::Middleware.valid?+) can be registered.
     def validate_registration!(name, middleware)
       super
       raise Middlegem::InvalidMiddlewareError unless Middlegem::Middleware.valid?(middleware)
