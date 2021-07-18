6d85833d71303e747d279293c3e2fd69f952a1c7
Jacob <jacoblockard99@gmail.com>
2021-07-18 16:23:45 -0400

    Remove tests directory
    Remove the 'test' directory that somehow got added.

--- test/edit_in_place_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class EditInPlaceTest < ActiveSupport::TestCase
-  test 'it has a version number' do
-    assert EditInPlace::VERSION
-  end
-end
